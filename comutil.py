f7cb583972360daf33a9bcf97bd9fb655f8a7b1e
qingping <qingpingjian1985@163.com>
2018-04-12 20:59:27 +0800

    Continue to Pattern detect demo

--- comutil.py
+++ comutil.py
@@ -9,11 +9,42 @@ Created on 2018/4/12 11:09
 """
 import numpy as np
 
+from anglefunc import angleNormalize
+
+def slidingWindowFilter(timeList, valueList, windowSize):
+    midPos = (windowSize - 1) / 2
+    currentIndex = 0
+    dataLength = np.min((len(timeList), len(valueList)))
+    timeFList = timeList[0:dataLength]
+    valueFList = []
+    valueFList.extend(valueList[currentIndex:currentIndex + midPos])
+    valueFList.extend([np.mean(valueList[i - midPos : i + midPos + 1])
+                       for i in range(currentIndex + midPos, dataLength - midPos)])
+    valueFList.extend(valueList[dataLength - midPos:dataLength])
+    return timeFList, valueFList
+
 def varOfAcce(timeList, valueList, windowSize):
     midPos = (windowSize - 1) / 2
     votList = [timeList[i] for i in range(midPos, len(valueList) - midPos)]
     varList = [np.var(valueList[i - midPos:i + midPos + 1]) for i in range(midPos, len(valueList) - midPos)]
     return votList, varList
+
+def rotationAngle(gyroTimeList, gyroValueList, normalize = True):
+    """
+    clockwise rotation return position values and keep rotation angle in {0, 2pi) based on normalize flag
+    :param gyroTimeList: Gyroscope data timestamp
+    :param gyroValueList: Gyroscope data list
+    :param normalize: normalize flag
+    :return: rotation angle in radian
+    """
+    # Between two timestamps, we use the average value as the real rate.
+    avgList = [gyroValueList[0]]
+    avgList.extend([(gyroValueList[i - 1] + gyroValueList[i]) / 2.0 for i in range(1, len(gyroValueList))])
+    integrationList = [0.0]
+    for j in range(1, len(avgList)):
+        integrationList.append((gyroTimeList[j] - gyroTimeList[j - 1]) * avgList[j] + integrationList[j - 1])
+    # clockwise rotation return position values and keep rotation angle in {0, 2pi) based on circularData flag
+    return [angleNormalize(-1.0 * rot) if normalize else (-1.0 * rot) for rot in integrationList]
 
 if __name__ == "__main__":
     print("Done.")
--- patterndemo.py
+++ patterndemo.py
@@ -7,6 +7,103 @@ Created on 2018/4/12 10:29
 @file: patterndemo.py
 @software: PyCharm Community Edition
 """
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+
+from matplotlib.ticker import  MultipleLocator, FormatStrFormatter
+
+from comutil import *
+from dataloader import loadAcceData, loadGyroData
+
+# Environment Configuration
+matplotlib.rcParams['font.size'] = 15
+# matplotlib.rcParams['font.sans-serif'] = ['SimHei'] # 用来正常显示中文标签
+matplotlib.rcParams['font.sans-serif'] = ["simsun"] # 用来正常显示中文标签
+matplotlib.rcParams['axes.unicode_minus'] = False # 用来正常显示负号
 
 if __name__ == "__main__":
+    sensorFilePath = ("./Examples/PatternDetect/20170622153925_acce.csv",
+                      "./Examples/PatternDetect/20170622153925_gyro.csv")
+
+    # Load accelerometer data from files
+    acceTimeList, acceValueList = loadAcceData(sensorFilePath[0], combineFlag = True)
+    windowSize = 7
+    acceVotList, acceVarList = varOfAcce(acceTimeList, acceValueList, windowSize)
+
+    # Stationary
+    timeListForStat = acceVotList[51:341]
+    timeListForStat = [t - timeListForStat[0] for t in timeListForStat]
+    valueListForStat = acceVarList[51:341]
+
+    # Forward or Backward
+    timeListForWalk = acceVotList[1511:1801]
+    timeListForWalk = [t - timeListForWalk[0] for t in timeListForWalk]
+    valueListForWalk = acceVarList[1511:1801]
+
+    gyroTimeList, gyroValueList = loadGyroData(sensorFilePath[1])
+    windowSize = 21
+    gyroTimeFltList, gyroValueFltList = slidingWindowFilter(gyroTimeList, gyroValueList, windowSize)
+
+    # Normal motion
+    timeListForNW = gyroTimeFltList[1101:1601]
+    timeListForNW = [t - timeListForNW[0] for t in timeListForNW]
+    valueListForNW = gyroValueFltList[1101:1601]
+
+    # Turns
+    timeListForTurns = gyroTimeFltList[2581:3081]
+    timeListForTurns = [t - timeListForTurns[0] for t in timeListForTurns]
+    valueListForTurns = gyroValueFltList[2581:3081]
+
+    # Right and Left turn
+    timeListForLeft = gyroTimeList[1781:2281]
+    gyroListForLeft = gyroValueList[1781:2281]
+    timeListForLeft = [t - timeListForLeft[0] for t in timeListForLeft]
+    rotAngleListForLeft = rotationAngle(timeListForLeft, gyroListForLeft, normalize=False)
+    rotDegreeListForLeft = [r * 180.0 / math.pi for r in rotAngleListForLeft]
+
+    # Turn around
+    timeListForUTurn = gyroTimeList[2581:3081]
+    gyroListForUTurn = gyroValueList[2581:3081]
+    timeListForUTurn = [t - timeListForUTurn[0] for t in timeListForUTurn]
+    rotAngleListForUTurn = rotationAngle(timeListForUTurn, gyroListForUTurn, normalize=False)
+    rotDegreeListForUTurn = [r * -180.0 / math.pi for r in rotAngleListForUTurn]
+
+    fig = plt.figure()
+    statSeparateAxe = fig.add_subplot(111)
+    statSeparateAxe.set_ylim(0.0, 3.0)
+    statSeparateAxe.yaxis.set_major_locator(MultipleLocator(1.0))
+    statSeparateAxe.yaxis.set_major_formatter(FormatStrFormatter("%.1f"))
+    statSeparateAxe.yaxis.set_minor_locator(MultipleLocator(0.5))
+    # statSeparateAxe.set_xticks([]) # Hidden the x ticks
+    statSeparateAxe.set_ylabel(u"加速度方差")
+    statSeparateAxe.set_xlabel(u"时间(s)")
+    statSeparateAxe.plot(timeListForStat, valueListForStat, color="red", lw=2, linestyle="--", label=u"静止")
+    statSeparateAxe.plot(timeListForWalk, valueListForWalk, color="blue", lw=2, linestyle="-", label=u"直线运动")
+    plt.legend(loc=2)
+
+    # walkSeparateAxe = fig.add_subplot(312)
+    # walkSeparateAxe.set_ylim(-2.0, 2.0)
+    # walkSeparateAxe.yaxis.set_major_locator(MultipleLocator(1.0))
+    # walkSeparateAxe.yaxis.set_major_formatter(FormatStrFormatter("%.1f"))
+    # walkSeparateAxe.yaxis.set_minor_locator(MultipleLocator(0.5))
+    # walkSeparateAxe.set_xticks([]) # Hidden the x ticks
+    # walkSeparateAxe.set_ylabel("$Rot. Rat.$")
+    # walkSeparateAxe.plot(timeListForNW, valueListForNW, color="red", lw=7, linestyle="-", label="Normal Walking")
+    # walkSeparateAxe.plot(timeListForTurns, valueListForTurns, color="blue", lw=7, linestyle="--", label="Turns")
+    # plt.legend(loc=4)
+    #
+    # turnSeparateAxe = fig.add_subplot(313)
+    # turnSeparateAxe.set_ylim(-50, 200)
+    # turnSeparateAxe.yaxis.set_major_locator(MultipleLocator(50))
+    # turnSeparateAxe.yaxis.set_major_formatter(FormatStrFormatter("%d"))
+    # turnSeparateAxe.yaxis.set_minor_locator(MultipleLocator(25))
+    # turnSeparateAxe.set_xticks([]) # Hidden the x ticks
+    # turnSeparateAxe.set_ylabel("$Rot. Ang.$")
+    # turnSeparateAxe.plot(timeListForLeft, rotDegreeListForLeft, color="blue", lw=7, label="Left Turn")
+    # turnSeparateAxe.plot(timeListForUTurn, rotDegreeListForUTurn, color="red", lw=7, label="U-Turn", linestyle="--")
+    # plt.legend(loc=2)
+
+    plt.show()
+
     print("Done.")
